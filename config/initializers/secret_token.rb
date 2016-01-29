c2180b3b6d543f4bbe60334d56d5e141c97dd71b
Sherry Ladzinski <sherryladzinski@gmail.com>
2016-01-29 14:25:51 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Maplewedding::Application.config.secret_key_base = '5115350162b90e6bdfdb70096bf3a4f5bbc118e5b35026934802120d54dfe62f92b476d2ff4a57e2fea1fedbc769e2ac175746bea628d364fe1f42193e04e900'
